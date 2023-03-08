c39c9080f22037cb23c855400fd3f2e9cb43dfa2
xchoot <106496026+xchoot@users.noreply.github.com>
2023-03-08 14:23:44 +1100

    Add files via upload

---
+++ j_delivery.js
@@ -0,0 +1,8 @@
+function pickupCheck() {
+    document.getElementById("shipping").style.display = "none"
+}
+
+function shipCheck() {
+    document.getElementById("shipping").style.display = "block"
+}
+    
---
+++ j_search.js
@@ -0,0 +1,7 @@
+function printInput(searchResult) {
+    let print = document.getElementById("search").value
+    let searchResult = "SEARCHED FOR: " + print
+    console.log(searchResult)
+
+    return searchResult
+}
